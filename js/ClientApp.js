43f95353aaad2cc0c63c38113ab93ef086f89471
Yashwant Kumar <yashwant.kumar@practo.com>
2017-08-30 00:47:14 +0530

    2nd component

---
+++ js/ClientApp.js
@@ -0,0 +1,26 @@
+/*global ReactDome*/
+
+
+var div = React.DOM.div		
+var h1 = React.DOM.h1
+var MyTitle = React.createClass({
+	render: function() {
+		return(
+			div(null ,
+				h1(null, 'Checkout this component')
+			)
+			);
+	}
+
+})
+
+var MyFirstComponent = (
+		div(null,
+			React.createElement(MyTitle,null),
+			React.createElement(MyTitle,null),
+			React.createElement(MyTitle,null),
+		)
+
+	)
+
+ReactDom.render(MyFirstComponent,document.getElementById(''));
