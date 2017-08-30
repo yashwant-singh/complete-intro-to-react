7f6ece4b9604a6997f10ca48020da5947cd09cc5
Yashwant Kumar <yashwant.kumar@practo.com>
2017-08-30 15:48:00 +0530

    standard

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -3,30 +3,28 @@ var div = React.DOM.div
 var h1 = React.DOM.h1
 
 var MyTitle = React.createClass({
-	render: function() {
-		return (
-				div(null,
-						h1({style: {color: this.props.color}}, this.props.title)
-					)
-			)
-	}
+  render: function () {
+    return (
+        div(null,
+            h1({style: {color: this.props.color}}, this.props.title)
+        )
+    ) }
 })
 
 var MyTitleFact = React.createFactory(MyTitle)
 
 var MyFirstComponent = React.createClass({
-	render: function() {
-		return(
-			
-			div(null,
-					MyTitleFact({title : 'Props are great', color : 'rebeccapurple'}),
-					MyTitleFact({title : 'Use props every where', color : 'mediumaquamarine'}),
-					MyTitleFact({title : 'Props are the best', color : 'peru'})
-				)
-			
-			)
-			
-	}
+  render: function () {
+    return (
+
+        div(null,
+                MyTitleFact({title: 'Props are great', color: 'rebeccapurple'}),
+                MyTitleFact({title: 'Use props every where', color: 'mediumaquamarine'}),
+                MyTitleFact({title: 'Props are the best', color: 'peru'})
+            )
+
+        )
+  }
 
 })
-ReactDOM.render(React.createElement(MyFirstComponent),document.getElementById('app'))
+ReactDOM.render(React.createElement(MyFirstComponent), document.getElementById('app'))
