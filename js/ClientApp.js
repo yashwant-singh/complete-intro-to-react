ed28061c5ef149241b12046c4b4b73d0c1edb265
Yashwant Kumar <yashwant.kumar@practo.com>
2017-08-30 12:01:10 +0530

    third component

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,13 +1,29 @@
 
 var div = React.DOM.div
 var h1 = React.DOM.h1
+
+var MyTitle = React.createClass({
+	render: function() {
+		return (
+				div(null,
+						h1(null, 'check out this component')
+					)
+			)
+	}
+})
+
 var MyFirstComponent = React.createClass({
 	render: function() {
 		return(
-			div(null ,
-				h1(null, 'This is my first component')
+			
+			div(null,
+					React.createElement(MyTitle),
+					React.createElement(MyTitle),
+					React.createElement(MyTitle)
+				)
+			
 			)
-			);
+			
 	}
 
 })
