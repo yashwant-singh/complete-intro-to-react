ec0b3d843b2c834db55cbabf3a6fbd65ec43da7a
Yashwant Kumar <yashwant.kumar@practo.com>
2017-09-01 08:33:20 +0530

    Ex6 transformation through bablerc to webpack

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+
+module.exports = {
+		context: __dirname,
+		entry: './js/ClientApp.js',
+		devtool: 'source-map',
+		output: {
+				path: path.join(__dirname, '/public'),
+				filename: 'bundle.js'
+		},
+		resolve: {
+				extensions: ['.js', '.json']
+		},
+		stats: {
+				colors: true,
+				reasons: true,
+				chunks: false
+		},
+		module: {
+				rules: [
+						{
+							test: /\.js$/,
+							exclude: /(node_modules|bower_components)/,
+							use: {
+								  loader: 'babel-loader',
+							      options: {
+							      presets: ['env']
+								}
+							}
+							
+						}
+					]
+		}
+}
+
