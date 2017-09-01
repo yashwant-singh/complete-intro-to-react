1c632bbd79a0252eecfca4b0ef08787aa0675227
Yashwant Kumar <yashwant.kumar@practo.com>
2017-09-01 11:29:07 +0530

    babelrc and webpack

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,16 +1,9 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import MyTitle from "./MyTitle"
 var div = React.DOM.div
 var h1 = React.DOM.h1
 
-var MyTitle = React.createClass({
-  render: function () {
-    return (
-        div(null,
-            h1({style: {color: this.props.color}}, this.props.title)
-        )
-    ) }
-})
 
 var MyTitleFact = React.createFactory(MyTitle)
 
