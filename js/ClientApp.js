9ef6038046233c34fa7ea247cfa21aa5a08bdb77
Yashwant Kumar <yashwant.kumar@practo.com>
2017-09-04 10:47:23 +0530

    BrowserRoute

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { HashRouter, Match } from 'react-router'
+import { BrowserRouter, Match } from 'react-router'
 import Landing from './Landing'
 import Search from './Search'
 import EmpInfo from './EmpInfo'
@@ -10,13 +10,13 @@ import '../public/style.css'
 const App = React.createClass({
   render () {
     return (
-      <HashRouter>
+      <BrowserRouter>
         <div className='app'>
           <Match exactly pattern='/' component={Landing} />
           <Match pattern='/search' component={Search} />
-          <Match pattern='/empinfo' component={EmpInfo}/>
+          <Match pattern='/empinfo' component={EmpInfo} />
         </div>
-      </HashRouter>
+      </BrowserRouter>
     )
   }
 })
--- js/EmpInfo.js
+++ js/EmpInfo.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const EmpInfo = React.createClass({
-	render() {
-		return(
-			<h1> Title</h1>
-		)
-	}
+  render () {
+    return (
+      <h1> Title</h1>
+    )
+  }
 })
 
 export default EmpInfo
