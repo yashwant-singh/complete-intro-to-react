4b9898876447a6a6820dc3990fd3918a292acb63
Yashwant Kumar <yashwant.kumar@practo.com>
2017-09-05 01:36:15 +0530

    fileter

--- js/Search.js
+++ js/Search.js
@@ -4,15 +4,30 @@ import preload from '../public/data.json'
 import ShowCard from './ShowCard'
 
 const Search = React.createClass({
+  getInitialState () {
+    return {
+      searchTerm: 'this is the default searchTerm'
+    }
+  },
+  handleSearchTermXhange (event) {
+    this.setState({searchTerm: event.target.value})
+  },
   render () {
     console.log(preload.shows)
     return (
       <div>
-        {preload.shows.map((show) => {
-          return (
-            <ShowCard {...show} />
-           )
-        })}
+        <header>
+          <h1>{this.state.searchTerm}</h1>
+          <input type='text' placeholder='Search' value={this.state.searchTerm} onChange={this.handleSearchTermXhange} />
+        </header>
+        <div>
+          {preload.shows.filter((show) => `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase) >= 0).map((show, index) => {
+            return (
+              <ShowCard {...show} key={index} id={index} />
+            )
+          })
+        }
+        </div>
       </div>
       )
   }
