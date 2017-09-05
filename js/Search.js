cef6933fa76a57961ddb8c9f794a15777234b4c8
Yashwant Kumar <yashwant.kumar@practo.com>
2017-09-05 11:59:03 +0530

    empty initial state

--- js/Search.js
+++ js/Search.js
@@ -6,7 +6,7 @@ import ShowCard from './ShowCard'
 const Search = React.createClass({
   getInitialState () {
     return {
-      searchTerm: 'this is the default searchTerm'
+      searchTerm: ''
     }
   },
   handleSearchTermXhange (event) {
