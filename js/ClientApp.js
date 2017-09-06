952234b209640536ea03fc805101a1e8a2e8eca0
Yashwant Kumar <yashwant.kumar@practo.com>
2017-09-06 22:51:55 +0530

    Landing redirecting to search

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { HashRouter, Match } from 'react-router'
+import { Provider } from 'react-redux'
+import store from './store'
 import Landing from './Landing'
 import Search from './Search'
 import EmpInfo from './EmpInfo'
@@ -20,18 +22,20 @@ const App = React.createClass({
   render () {
     return (
       <HashRouter>
-        <div className='app'>
-          <Match exactly pattern='/' component={Landing} />
-          <Match pattern='/search' component={(props) => { return <Search shows={preload.shows} {...props} /> }
-        }
-        />
-          <Match pattern='/empinfo' component={EmpInfo} />
-          <Match pattern='/details/:id' component={(props) => {
-            const show = preload.shows.filter((show) =>
-              props.params.id === show.imdbID)
-            return <Details show={show[0]} {...props} />
-          }} />
-        </div>
+        <Provider store={store}>
+          <div className='app'>
+            <Match exactly pattern='/' component={Landing} />
+            <Match pattern='/search' component={(props) => { return <Search shows={preload.shows} {...props} /> }
+          }
+          />
+            <Match pattern='/empinfo' component={EmpInfo} />
+            <Match pattern='/details/:id' component={(props) => {
+              const show = preload.shows.filter((show) =>
+                props.params.id === show.imdbID)
+              return <Details show={show[0]} {...props} />
+            }} />
+          </div>
+        </Provider>
       </HashRouter>
     )
   }
--- js/Landing.js
+++ js/Landing.js
@@ -1,15 +1,56 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { setSearchTerm } from './actionCreators'
 
+const { string, func, object } = React.PropTypes
 const Landing = React.createClass({
+
+  contextTypes: {
+    router: object
+  },
+  propTypes: {
+    searchTerm: string,
+    dispatchSetSearchTerm: func
+  },
+
+  handleSearchTermChange (event) {
+    this.props.dispatchSetSearchTerm(event.target.value)
+  },
+
+  // handleSearchTermChange (event) {
+  //   this.props.dispatch(setSearchTerm(event.target.value))
+  // },
+
+  handleSearchSubmit (event) {
+    event.preventDefault()
+    this.context.router.transitionTo('/search')
+  },
+
   render () {
     return (
       <div className='landing'>
         <h1>svideo</h1>
-        <input type='text' placeholder='Search' />
+        <form onSubmit={this.handleSearchSubmit}>
+          <input onChange={this.handleSearchTermChange} value={this.props.searchTerm} type='text' placeholder='Search' />
+        </form>
         <a>or Browse All</a>
       </div>
     )
   }
 })
 
-export default Landing
+const mapStateToProps = (state) => {
+  return {
+    searchTerm: state.searchTerm
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    dispatchSetSearchTerm (searchTerm) {
+      dispatch(setSearchTerm(searchTerm))
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Landing)
