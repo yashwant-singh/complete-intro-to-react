de2d40218424810e0bb7c0a005aa578c068a4ff9
Yashwant Kumar <yashwant.kumar@practo.com>
2017-09-17 16:25:59 +0530

    Dispatching from landing to search

--- js/Landing.js
+++ js/Landing.js
@@ -1,18 +1,35 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router'
-const { string } = React.PropTypes
+import { setSearcTerm } from './actionCreators'
+const { string, func, object } = React.PropTypes
 
 const Landing = React.createClass({
-  propTypes: {
-    searchTerm: string
+  contextTypes: {
+    router: object
   },
+  propTypes: {
+    searchTerm: string,
+    dispatch: func,
+    dispatchSetSearcTerm: func
+  },
+
+  handleSearchTermChange (event) {
+    this.props.dispatchSetSearcTerm(event.target.value)
+  },
+  handleSubmit (event) {
+    event.preventDefult()
+    this.contextTypes.router.transitionTo('/search')
+  },
+
   render () {
     console.log('hi')
     return (
       <div className='landing'>
         <h1>svideo</h1>
-        <input value={this.props.searchTerm} type='text' placeholder='Search' />
+        <form onSubmit={this.handleSubmit}>
+          <input onChange={this.handleSearchTermChange} value={this.props.searchTerm} type='text' placeholder='Search' />
+        </form>
         <Link to='/search'>or Browse All</Link>
       </div>
     )
@@ -24,5 +41,14 @@ const mapStateToProps = state => {
     searchTerm: state.searchTerm
   }
 }
-export default connect(mapStateToProps)(Landing)
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    dispatchSetSearcTerm (searchTerm) {
+      dispatch(setSearcTerm(searchTerm))
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Landing)
 
